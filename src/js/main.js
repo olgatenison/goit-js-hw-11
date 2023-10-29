0a8ad41e5eba3f664b4e70d795f2b2dde43669a6
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-10-29 17:20:23 +0200

    some styles

--- src/js/main.js
+++ src/js/main.js
@@ -101,10 +101,10 @@ function createMarkup(cards) {
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
       </a>
         <div class="info">
-          <p class="info-item"><b>${likes}</b></p>
-          <p class="info-item"><b>${views}</b></p>
-          <p class="info-item"><b>${comments}</b></p>
-          <p class="info-item"><b>${downloads}</b></p>
+          <p class="info-item">likes: <b>${likes}</b></p>
+          <p class="info-item">views: <b>${views}</b></p>
+          <p class="info-item">comments: <b>${comments}</b></p>
+          <p class="info-item">downloads: <b>${downloads}</b></p>
         </div>
       </div>
     `;
